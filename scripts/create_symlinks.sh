7cc721dcb15540d6f14c0a66da5ed5b4a2d90756
Simon Innes <simoninnesz@gmail.com>
2023-06-20 20:29:16 -0700

    trying something

--- scripts/create_symlinks.sh
+++ scripts/create_symlinks.sh
@@ -2,4 +2,5 @@ ln -sf ~/github-repos/dotfiles/bashrc ~/.bashrc
 ln -sf ~/github-repos/dotfiles/bash_profile ~/.bash_profile
 ln -sf ~/github-repos/dotfiles/vimrc ~/.vimrc
 ln -sf ~/github-repos/dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/github-repos/dotfiles/test ~/.test
 ln -sf ~/github-repos/dotfiles/tmux.conf ~/.tmux.conf
