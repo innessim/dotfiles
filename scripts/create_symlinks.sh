1c0a943219ec05700b1bf545057ef2480f6070a4
Simon Innes <simoninnesz@gmail.com>
2023-06-20 20:37:31 -0700

    gave up on test

--- scripts/create_symlinks.sh
+++ scripts/create_symlinks.sh
@@ -2,5 +2,4 @@ ln -sf ~/github-repos/dotfiles/bashrc ~/.bashrc
 ln -sf ~/github-repos/dotfiles/bash_profile ~/.bash_profile
 ln -sf ~/github-repos/dotfiles/vimrc ~/.vimrc
 ln -sf ~/github-repos/dotfiles/gitconfig ~/.gitconfig
-ln -sf ~/github-repos/dotfiles/test ~/.test
 ln -sf ~/github-repos/dotfiles/tmux.conf ~/.tmux.conf
