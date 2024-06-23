44dad4969288009ca15dcf608890a2714b7aa4f1
Simon Innes <simoninnesz@gmail.com>
2024-06-23 14:35:53 -0500

    fix symlink of vim/colors

--- scripts/create_symlinks.sh
+++ scripts/create_symlinks.sh
@@ -4,4 +4,4 @@ ln -sf ~/github-repos/dotfiles/vimrc ~/.vimrc
 ln -sf ~/github-repos/dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/github-repos/dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/github-repos/dotfiles/bootstrap ~/.bootstrap
-ln -sf ~/github-repos/dotfiles/vim/colors ~/.vim/colors
+ln -sf ~/github-repos/dotfiles/vim/colors ~/.vim
